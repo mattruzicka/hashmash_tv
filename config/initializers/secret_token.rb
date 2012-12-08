c4d187ea9c719ce8b3635119f7d536faee47e363
Matt Ruzicka <mjrguitar@gmail.com>
2012-12-08 02:59:17 -0500

    hashmash.tv/#tag/feed/@user/feed/feed

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hashmashtv::Application.config.secret_token = '7e0dc0fce41fa29c726c0b8ac93e0018d6bb8d07f58d3c0756de3e668228791dfdad248e71a1060d1021ffe340dcf9b4c201c954edded71515ea3556b21df5a8'
