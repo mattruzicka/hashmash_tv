4995109a44f86b72bad014f69248d2d6d870e40a
Matt Ruzicka <mjrguitar@gmail.com>
2012-12-08 21:54:18 -0500

    nvm fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   
-  # before_filter :ensure_domain if Rails.env.production?
-  # 
-  # def ensure_domain 
-  #   domain = 'www.hashmash.tv'
-  # 
-  #   if request.host != domain 
-  #     redirect_to request.protocol + domain
-  #   end
-  # end
+  before_filter :ensure_domain if Rails.env.production?
+
+  def ensure_domain 
+    domain = 'www.hashmash.tv'
+
+    if request.host != domain 
+      redirect_to request.protocol + domain
+    end
+  end
 end
