4981de495e67270830b714e67d33cec4e7efa012
Matt Ruzicka <mjrguitar@gmail.com>
2013-02-02 02:44:27 -0600

    env config

--- config/initializers/oauth.rb
+++ config/initializers/oauth.rb
@@ -3,8 +3,8 @@ require 'instagram'
 OAUTH = YAML.load(File.open("#{::Rails.root}/config/oauth.yml").read)
 
 Instagram.configure do |config|
-  config.client_id = OAUTH['instagram']['key']
-  config.client_secret = OAUTH['instagram']['secret']
+  config.client_id = ENV['instagram']['key']
+  config.client_secret = ENV['instagram']['secret']
 end
 
 INSTAGRAM_CALLBACK_URL = "http://localhost:3000/instagram"
