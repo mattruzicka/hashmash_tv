6efb88952e0f7ff6be486b30e5ac0145ec49c743
Matt Ruzicka <mjrguitar@gmail.com>
2013-03-20 18:36:47 -0500

    requiring not existent files = bad

--- config/initializers/oauth.rb
+++ config/initializers/oauth.rb
@@ -1,7 +1,5 @@
 require 'instagram'
 
-OAUTH = YAML.load(File.open("#{::Rails.root}/config/oauth.yml").read)
-
 Instagram.configure do |config|
   config.client_id = ENV['INSTAGRAM_KEY']
   config.client_secret = ENV['INSTAGRAM_SECRET']
