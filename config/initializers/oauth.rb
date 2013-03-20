d61e53715fc60bcb8f9c052d280ae96ccef8d9a0
Matt Ruzicka <mjrguitar@gmail.com>
2013-03-20 18:29:34 -0500

    match heroku env variables

--- config/initializers/oauth.rb
+++ config/initializers/oauth.rb
@@ -3,8 +3,8 @@ require 'instagram'
 OAUTH = YAML.load(File.open("#{::Rails.root}/config/oauth.yml").read)
 
 Instagram.configure do |config|
-  config.client_id = ENV['instagram']['key']
-  config.client_secret = ENV['instagram']['secret']
+  config.client_id = ENV['INSTAGRAM_KEY']
+  config.client_secret = ENV['INSTAGRAM_SECRET']
 end
 
 INSTAGRAM_CALLBACK_URL = "http://localhost:3000/instagram"
